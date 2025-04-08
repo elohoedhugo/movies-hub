b232cc78fb758261d0732e9daeb235d85d8f67ce
elohoedhugo <elohoedhugo@gmail.com>
2025-04-08 20:35:01 +0100

    made changes to responsiveness

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import logo from "../assets/logo-light-transparent.png"
 import "../App.css"
@@ -6,10 +6,16 @@ import { BiCameraMovie } from "react-icons/bi";
 import { LuTv } from "react-icons/lu";
 import { FaPlus } from "react-icons/fa6";
 import { MovieContext } from './MovieContext.jsx';
+import { CgMenuGridO } from "react-icons/cg";
 
 const RootLayout = () => {
 
   const {search, setSearch, searchMovies} = useContext(MovieContext)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+     setIsOpen(prev=>!prev)
+  }
   return (
     <div>
 
@@ -18,12 +24,28 @@ const RootLayout = () => {
         <nav>
          <div className='navlink'><BiCameraMovie className='react-icon' /><NavLink to="/">Movies</NavLink></div>
          <div className='navlink'><LuTv className='react-icon' /><NavLink>TV shows</NavLink></div>
-         <div className='navlink'><FaPlus className='react-icon' /><NavLink to="recommendations">Recommendations</NavLink></div>
-         <div className='inputdiv'><input placeholder='🔍     search for movies' type='text' 
-         value={search} onChange={(e)=> setSearch(e.target.value)}/></div>
+         <div className='navlink' id='recommendations'><FaPlus className='react-icon' /><NavLink to="recommendations">Recommendations</NavLink></div>
+         <input placeholder='🔍     search for movies' type='text' 
+         value={search} onChange={(e)=> setSearch(e.target.value)}/>
          <button className='searchbutton' onClick={searchMovies}>Search</button>
          <div className='navlink'><NavLink>My Account</NavLink></div>
+         
         </nav>
+        <div className='menu-button-div'>
+        <button className='menu' onClick={toggleMenu}><CgMenuGridO className='menu-icon'/></button>
+
+        {isOpen && (
+          <div className='menu-container'>
+           <ul>
+            <li>Movies</li>
+            <li>TV Shows</li>
+            <li>Recommendations</li>
+            <li>Account</li>
+           </ul>
+
+          </div>
+        )}
+        </div>
       </header>
 
       <main>
